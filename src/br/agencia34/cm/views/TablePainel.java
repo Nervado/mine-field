4a6918aba3d5a9776802a562832623322ba541a0
Nervado <evandroabreudeoliveira@gmail.com>
2021-03-27 15:46:40 -0300

    feat: add Lose and Win icons to JOptionPane

--- src/br/agencia34/cm/views/TablePainel.java
+++ src/br/agencia34/cm/views/TablePainel.java
@@ -2,6 +2,8 @@ package br.agencia34.cm.views;
 
 import java.awt.GridLayout;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
@@ -10,8 +12,12 @@ import br.agencia34.cm.model.Table;
 
 @SuppressWarnings("serial")
 public class TablePainel extends JPanel {
-  public TablePainel(Table table) {
 
+  private final Icon mineField = new ImageIcon(getClass().getResource("/resources/minefield.png"));
+
+  private final Icon lose = new ImageIcon(getClass().getResource("/resources/lose.png"));
+
+  public TablePainel(Table table) {
     setLayout(new GridLayout(table.getLins(), table.getCols()));
 
     table.forEachField(f -> add(new FieldButton(f)));
@@ -20,9 +26,9 @@ public class TablePainel extends JPanel {
 
       SwingUtilities.invokeLater(() -> {
         if (e.isWinner()) {
-          JOptionPane.showMessageDialog(this, "You Win :)");
+          JOptionPane.showMessageDialog(this, "You Win :)", "Mine Field", JOptionPane.ERROR_MESSAGE, mineField);
         } else {
-          JOptionPane.showMessageDialog(this, "You Lose :(");
+          JOptionPane.showMessageDialog(this, "You Lose :(", "Mine Field", JOptionPane.ERROR_MESSAGE, lose);
         }
 
         table.reset();
